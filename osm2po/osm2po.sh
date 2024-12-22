d30553c4d211e9139fd4056e092464a0a85dfb35
dkmak02 <dkmak02@gmail.com>
2024-12-22 19:12:53 +0100

    refactored and changed testData

--- osm2po/osm2po.sh
+++ osm2po/osm2po.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-# Dependencies installation
 echo "Installing dependencies..."
 if ! dpkg -l | grep -qw postgresql; then
   echo "PostgreSQL is not installed. Installing dependencies..."
@@ -11,7 +8,7 @@ else
   echo "PostgreSQL is already installed. Skipping installation."
 fi
 
-# Check if PostgreSQL client is already installed
+
 if ! dpkg -l | grep -qw postgresql-client; then
   echo "PostgreSQL client is not installed. Installing..."
   apt-get update && apt-get install -y postgresql-client
@@ -19,30 +16,28 @@ else
   echo "PostgreSQL client is already installed. Skipping installation."
 fi
 
-# Define paths and PostgreSQL credentials
+
 OSM2PO_JAR="osm2po-core-5.5.11-signed.jar"
-OSM_FILE="/osm_data/data.osm.pbf"  # Manually downloaded OSM file
+OSM_FILE="/osm_data/data.osm.pbf" 
 TILE_SIZE="x"
 PREFIX="hh"
 SQL_FILE_PATH="/usr/src/app/hh/hh_2po_4pgr.sql"
 
-# PostgreSQL environment variables
-POSTGRES_HOST="trip-planner-db"  # PostgreSQL container name
-POSTGRES_USER="tripplanner"
+
+POSTGRES_HOST="trip-planner-db"  
 POSTGRES_PASSWORD="yourpassword"
 POSTGRES_DB="tripplanner_db"
 
-# Check if the OSM file exists
+
 if [ ! -f "$OSM_FILE" ]; then
   echo "Error: OSM file ($OSM_FILE) not found. Please ensure it exists."
   exit 1
 fi
 
-# Run osm2po
+
 echo "Running osm2po with the manually downloaded OSM file..."
 java -Xmx1g -jar $OSM2PO_JAR prefix=$PREFIX tileSize=$TILE_SIZE $OSM_FILE postp.0.class=de.cm.osm2po.plugins.postp.PgRoutingWriter &
 
-# Wait for osm2po to finish generating the SQL file
 echo "Waiting for osm2po to generate the SQL file..."
 while [ ! -f $SQL_FILE_PATH ]; do
   echo "SQL file not found. Waiting..."
@@ -50,11 +45,11 @@ while [ ! -f $SQL_FILE_PATH ]; do
 done
 echo "SQL file generated successfully."
 
-# Importing OSM data into PostgreSQL
+
 echo "Importing OSM data into PostgreSQL..."
 PGPASSWORD="$POSTGRES_PASSWORD" psql -h "$POSTGRES_HOST" -p "5432" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f "$SQL_FILE_PATH"
 
-# Check if the import was successful
+
 if [ $? -eq 0 ]; then
   echo "OSM data successfully imported into PostgreSQL."
 else
